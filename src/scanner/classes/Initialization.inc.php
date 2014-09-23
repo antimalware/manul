1cc4348cea4449f7e0c861c8fdea3403fac16120
Greg Zemskov <audit@revisium.com>
2014-09-23 15:48:38 +0400

    Added startup check for document root readability

--- src/scanner/classes/Initialization.inc.php
+++ src/scanner/classes/Initialization.inc.php
@@ -10,25 +10,28 @@ $php_version_ok = true;
 $zip_ok = true;
 $dom_ok = true;
 $perm_ok = true;
+$php_root_readable = true;
 
+if (!is_readable($_SERVER['DOCUMENT_ROOT'])) {
+    $php_root_readable = false;
+    $critical_err = true;
+}
+     
 if (version_compare(phpversion(), '5.2.0', '<')) {
     $php_version_ok = false;
     $critical_err = true;
-    //die(PS_OLD_PHP);
 }
      
 $zipModuleVersion = phpversion('zip');
 if (empty($zipModuleVersion)) {
     $zip_ok = false;
     $critical_err = true;
-//    die(PS_NO_ZIP_MODULE);
 } 
 
 $domDocumentVersion = phpversion('dom');
 if (empty($domDocumentVersion)) {
     $dom_ok = false;
     $critical_err = true;
-//    die(PS_NO_DOMDOCUMENT_MODULE);
 } 
 
 $project_root_dir =  dirname(dirname(__FILE__));
@@ -46,7 +49,6 @@ if (!is_writable($project_tmp_dir)) {
           if (!is_writable($project_tmp_dir)) {
     		$perm_ok = false;
 		$critical_err = true;
-//             die(PS_ERR_NO_TEMP_FOLDER);
           }
       }
    }
@@ -55,16 +57,19 @@ if (!is_writable($project_tmp_dir)) {
 if ($critical_err)
  {
    $view = new View();
+   define('PS_CHECKER_ROOT_READABLE_STATUS', $php_root_readable ? PS_CHECKER_PASSED : PS_CHECKER_FAILED);
    define('PS_CHECKER_PHPVERSION_STATUS', $php_version_ok ? PS_CHECKER_PASSED : PS_CHECKER_FAILED);
    define('PS_CHECKER_ZIP_STATUS', $zip_ok ? PS_CHECKER_PASSED : PS_CHECKER_FAILED);
    define('PS_CHECKER_DOM_STATUS', $dom_ok ? PS_CHECKER_PASSED : PS_CHECKER_FAILED);
    define('PS_CHECKER_PERM_STATUS', $perm_ok ? PS_CHECKER_PASSED : PS_CHECKER_FAILED);
 
+   define('PS_CHECKER_ROOT_READABLE_STYLE', $php_root_readable ? 'label_green' : 'label_red');
    define('PS_CHECKER_PHPVERSION_STYLE', $php_version_ok ? 'label_green' : 'label_red');
    define('PS_CHECKER_ZIP_STYLE', $zip_ok ? 'label_green' : 'label_red');
    define('PS_CHECKER_DOM_STYLE', $dom_ok ? 'label_green' : 'label_red');
    define('PS_CHECKER_PERM_STYLE', $perm_ok ? 'label_green' : 'label_red');
 
+   define('PS_CHECKER_ROOT_READABLE_STYLE', $php_root_readable ? 'none' : 'block');
    define('PS_CHECKER_PHPVERSION_FIX', $php_version_ok ? 'none' : 'block');
    define('PS_CHECKER_ZIP_FIX', $zip_ok ? 'none' : 'block');
    define('PS_CHECKER_DOM_FIX', $dom_ok ? 'none' : 'block');
