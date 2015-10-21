9ac26e5e9c99b5cf9ef98e385ac8b572aa500e4f
peevo <peevo@yandex-team.ru>
2015-10-21 05:39:21 +0300

    id and signature in scan log

---
+++ src/scanner/classes/SignatureDetect.inc.php
@@ -0,0 +1,51 @@
+<?php
+
+ob_start();
+require_once('Tskv.inc.php');
+ob_end_clean();
+
+
+class SignatureDetect
+{
+    function __construct($filePath, $isDetected, $id, $severity, $signature, $position, $detectedContent) 
+    { 
+
+        $this->filePath = $filePath;    
+        $this->isDetected = $isDetected;    
+        $this->id = $id;    
+        $this->severity = $severity;
+        $this->signature = $signature;
+        $this->position = $position;
+        $this->detectedContent = $detectedContent;
+    }
+
+    function toString() 
+    {
+        $elements = array('filePath' => $this->filePath,
+                          'isDetected' => $this->isDetected, 
+                         );
+        if ($this->isDetected == 1) {
+            $elements['id'] = $this->id;
+            $elements['severity'] = $this->severity;
+            $elements['signature'] = $this->signature;
+            $elements['position'] = $this->position;
+            $elements['detectedContent'] = $this->detectedContent;
+        }
+        return toTskv($elements);
+    }
+
+    function fromString($tskvString) 
+    {
+         $keyValuePairs = fromTskv($tskvString);
+         $this->filePath = $keyValuePairs['filePath'];    
+         $this->isDetected = $keyValuePairs['isDetected']; 
+         if ($this->isDetected == 1) {   
+             $this->id = $keyValuePairs['id'];    
+             $this->severity = $keyValuePairs['severity'];
+             $this->signature = $keyValuePairs['signature'];
+             $this->position = $keyValuePairs['position'];
+             $this->detectedContent = $keyValuePairs['detectedContent'];
+         }
+    }
+    
+}
